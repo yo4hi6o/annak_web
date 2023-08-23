614d445976a41580fc0661471ba915204f3cac55
yo4hi6o <yoshihiro-kanna@japanmaterial.ne.jp>
2023-08-23 11:25:40 +0900

    Add contact form[

---
+++ theme/contact/em_laravel/resources/views/theme/gray/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="fixed-bottom">
+  @isset($append['in_fixed_bottom_top']){!! $append['in_fixed_bottom_top'] !!}@endisset
+  <div class="col-12 bg-dark text-white text-center p-2">
+    ANNAK.co.ltd
+  </div>
+  @isset($append['in_fixed_bottom_bottom']){!! $append['in_fixed_bottom_bottom'] !!}@endisset
+</footer>
+
---
+++ theme/contact/em_laravel/resources/views/theme/gray/header.blade.php
@@ -0,0 +1,9 @@
+<header>
+  @isset($append['in_header_top']){!! $append['in_header_top'] !!}@endisset
+  <div class="col-12 bg-dark text-white text-center p-5">
+
+    <h1>ANNAK</h1>
+
+  </div>
+  @isset($append['in_header_bottom']){!! $append['in_header_bottom'] !!}@endisset
+</header>
